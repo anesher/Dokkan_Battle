781076ca2fa06b21265a3111a722b992b7de69dc
felipejimenez2ds-admin <jimenezluquefelipe@gmail.com>
2025-02-20 17:59:49 +0100

    subida video, index.php y estilos

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dokkan Battle</title>
+    <link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+    <div class="main">
+    <video autoplay loop muted playsinline>
+            <source src="./videos/Dragon Ball Sparking Zero Opening Intro Animation 4K.mp4">
+        </video>
+    <header>
+        <button class="registro"><strong>REGISTRARSE</strong></buttons>
+
+        <button class="login"><strong>LOGIN<strong></button>
+    </header>
+
+    
+    
+        
+    </div>
+    
+</body>
+</html>

f686ee4463c63ead7740b062edcf26d44be05a58
antonio.eslava <anesher2002@gmail.com>
2025-03-24 17:12:58 +0100

    subida

--- class/usuario.php
+++ class/usuario.php
@@ -94,6 +94,7 @@ class Usuario
 
     public function setPiedras($piedras)
     {
+        // para conseguir las piedras que tiene el usuario
         $this->piedras = $piedras;
     }
 
@@ -149,6 +150,7 @@ class Usuario
 
     // Función para iniciar sesión con el usuario y la contraseña
     public function login() {
+        session_start();
         $conn = connect_bbdd();
         $consulta = "SELECT * FROM usuario WHERE nombre_usuario = ?";
         $stmt = $conn->prepare($consulta);
@@ -167,6 +169,7 @@ class Usuario
             }
         }
         return false;
+        
     }
 
     // Función para conseguir una piedra
@@ -186,6 +189,15 @@ class Usuario
         $_SESSION['piedras'] = $nuevasPiedras;
         
         return $nuevasPiedras;
+    }
+    // funcion para conseguir un personaje aleatorio 
+    public function obtenerPersonajeAleatorio() {
+        $query = "SELECT id, name, ki, maxKi, race, gender, description, image, affiliation FROM cartas WHERE deletedAt IS NULL ORDER BY RAND() LIMIT 1";
+        $stmt = $this->db->prepare($query);
+        $stmt->execute();
+        $personaje = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        return $personaje ?: null;
     }
 }
 
